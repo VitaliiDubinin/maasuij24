d3611511f1f7e60ddbbde2d35158e8c61a57d861
Vitalii Dubinin <veresk@siltatel.fi>
2024-06-04 23:19:22 +0300

    infoBox modified

--- src/components/mapbox/InfoBox.jsx
+++ src/components/mapbox/InfoBox.jsx
@@ -3,6 +3,16 @@ import React from "react";
 const InfoBox = ({ deleteSelectedLine, deleteSelectedPoints, selectedLineId, selectedPoints }) => {
   return (
     <div className="info-box">
+
+<p>
+      Click on two points, one by one, to draw a line between them. Use the draw tools to add points.
+    </p>
+    <p>
+      Click on a line or point to select it and delete then. 
+    </p>
+
+
+
       {selectedLineId && (
         <button onClick={deleteSelectedLine}>Delete Selected Line</button>
       )}
